ab2637e46e82c3ff172cb7101e2f690521773ccf
Végh Dani <danielvegh861@gmail.com>
2026-02-19 10:04:42 +0100

    vlami2

--- feladat_004.py
+++ feladat_004.py
@@ -12,17 +12,58 @@ try:
         
         kapcsolat.send_config_set("login block-for 600 attempts 3 within 60")
 
-        passwu = kapcsolat.send_command("show run | section username")
         passwe = kapcsolat.send_command("show run | sec enable")
-        passwc = kapcsolat.send_command("show run | sec line con")
-        passwv = kapcsolat.send_command("show run | sec line vty")
         
-        passwe = passwu.strip().split()[-1]
+        passwe = passwe.strip().split()[-1]
         if len(passwe) < 8:
-            jelszo = input("Adj meg egy jelszót ami minimum 8 karakter hosszú")
+            jelszo = input("Adj meg egy jelszót ami minimum 8 karakter hosszú:")
+            
             while len(jelszo) < 8:
-                jelszo = input("Adj meg egy jelszót ami minimum 8 karakter hosszú")
+                jelszo = input("Adj meg egy jelszót ami minimum 8 karakter hosszú:")
+            kapcsolat.send_config_set(f"enable password {jelszo}")
         
+        konzol = kapcsolat.send_command("sh run | section line con")
+        konzol = konzol.split("\n")
+        for i in konzol:
+            if i.strip().startswith('password'):
+                if len(i.split(' ')[-1]) < 8:
+                    ujjelszo2 = input("Adj meg egy legalább 8 karakterből álló jelszót: ")
+                
+                    while len(ujjelszo2) < 8:
+                        ujjelszo2 = input("Adj meg egy legalább 8 karakterből álló jelszót: ")
+                
+                    kapcsolat.send_config_set(["line con 0" , f"password {ujjelszo2}"])
+        
+        vty = kapcsolat.send_command("sh run | section line vty 0")
+        vty = vty.split("\n")
+        for i in vty:
+            if i.strip().startswith('password'):
+                if len(i.split(' ')[-1]) < 8:
+                    ujjelszo3 = input("Adj meg egy legalább 8 karakterből álló jelszót: ")
+                
+                    while len(ujjelszo3) < 8:
+                        ujjelszo3 = input("Adj meg egy legalább 8 karakterből álló jelszót: ")
+                
+                    kapcsolat.send_config_set(["line vty 0 15" , f"password {ujjelszo3}"])
+                    
+        user = kapcsolat.send_command("sh run | inc username")
+        user1 = user.strip().split(" ")
+        user = user.strip().split('\n')
+        print(user, user1)
+        
+        for i in user:
+            user = i.split(' ')[-1]
+        print(user)
+            
+        if len(user1[-1]) < 8:
+            ujjelszo4 = input("Adj meg egy legalább 8 karakterből álló jelszót: ")
+                    
+            while len(ujjelszo4) < 8:
+                ujjelszo4 = input("Adj meg egy legalább 8 karakterből álló jelszót: ")
+                    
+                    
+                    
+            kapcsolat.send_config_set(f"{ujjelszo4}")
         
         '''
         tftp_ip = input("Add meg a szerver IP-címét:")
